904279ff7ba9b672bec16caba827a88c10a05439
Habib-Amami <habibamami050@gmail.com>
2024-02-22 13:46:26 +0100

    adding logout button in the home for testing

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pfe_gmao/firebase_services.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => FirebaseService.instance.authInstance.signOut(),
+          child: const Icon(Icons.exit_to_app_outlined),
+        ),
+      ),
+    );
   }
 }
