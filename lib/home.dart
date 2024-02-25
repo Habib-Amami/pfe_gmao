ff7f9a3b513aab4a9a3558c736a6df0e52f3d307
Adam zahi <zahi.adem10@gmail.com>
2024-02-25 15:13:30 +0100

    adding menu to the application

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:pfe_gmao/firebase_services.dart';
+import 'menu screens/calender_screen.dart';
+import 'menu screens/equipment_screen.dart';
+import 'menu screens/notification_screen.dart';
+import 'menu screens/profile_screen.dart';
+import 'menu screens/work_order_screen.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentPageIndex = 1;
+  bool isDarkMode = false;
+  final List<Widget> menuScreens = const [
+    EquipmentScreen(),
+    CalenderScreen(),
+    WorkOrderScreen(),
+    NotificationScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => FirebaseService.instance.authInstance.signOut(),
-          child: const Icon(Icons.exit_to_app_outlined),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0x00e4eef4),
+        title: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 60,
+                width: 60,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: IconButton(
+                    icon: const Icon(Icons.person),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ProfileScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        onDestinationSelected: (int index) {
+          setState(
+            () {
+              currentPageIndex = index;
+            },
+          );
+        },
+        animationDuration: const Duration(
+          milliseconds: 500,
+        ),
+        selectedIndex: currentPageIndex,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.construction,
+            ),
+            icon: Icon(
+              Icons.construction_outlined,
+            ),
+            label: "Equipment",
+            tooltip: "Equipment List",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.tire_repair_outlined,
+            ),
+            icon: Icon(
+              Icons.tire_repair_outlined,
+            ),
+            label: "Intervention",
+            tooltip: "Tasks Calender",
+          ),
+          NavigationDestination(
+              selectedIcon: Icon(
+                Icons.webhook_outlined,
+              ),
+              icon: Icon(
+                Icons.webhook_rounded,
+              ),
+              label: "work order",
+              tooltip: "work flow order"),
+          NavigationDestination(
+            icon: Icon(
+              Icons.notifications,
+            ),
+            label: "Notifications",
+            tooltip: "Notifications",
+          ),
+        ],
+      ),
+      body: menuScreens[currentPageIndex],
     );
   }
 }
