8cabea621729c4009c4e718fd2f748d49b7810e6
Habib-Amami <habibamami050@gmail.com>
2024-02-25 14:58:08 +0100

    updating the login model adding saveCredentialsToLocalDB for saving suer emaila nd password in the cach adding getCachedEmail to get the user email cach adding getCachedPassword to get the user password

--- lib/features/authentication/login/model/login_model.dart
+++ lib/features/authentication/login/model/login_model.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../../../firebase_services.dart';
 
@@ -13,5 +18,27 @@ class LoginModel {
       password: password,
     );
     return userCredential.user;
+  }
+
+  Future<void> saveCredentialsToLocalDB({
+    required String email,
+    required String password,
+  }) async {
+    await Hive.box<String>("credentials").putAll(
+      {
+        "email": email,
+        "password": password,
+      },
+    );
+  }
+
+  String? getCachedEmail() {
+    final cachedEmail = Hive.box<String>("credentials").get("email");
+    return cachedEmail;
+  }
+
+  String? getCachedPassword() {
+    final cachedPassword = Hive.box<String>("credentials").get("password");
+    return cachedPassword;
   }
 }
