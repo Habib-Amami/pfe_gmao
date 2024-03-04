ca3a08cfd4bf7ea8900e48af296d4b4a2191db9f
Habib-Amami <habibamami050@gmail.com>
2024-03-04 18:53:48 +0100

    creating a model for the credential  caching

--- lib/features/authentication/login/model/login_remote_model.dart
+++ lib/features/authentication/login/model/login_remote_model.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-import '../../../../firebase_services.dart';
+import '../../../../firebase/firebase_services.dart';
 
 class RemoteLoginModel {
   Future<User?> login({
