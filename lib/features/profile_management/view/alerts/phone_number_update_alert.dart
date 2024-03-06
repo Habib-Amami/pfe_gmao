55d1e8879c8a7801fbb94cf245b0c9da721244fa
Habib-Amami <habibamami050@gmail.com>
2024-03-06 13:37:32 +0100

    adding comments

--- lib/features/profile_management/view/alerts/phone_number_update_alert.dart
+++ lib/features/profile_management/view/alerts/phone_number_update_alert.dart
@@ -11,29 +11,32 @@ class PhoneNumberUpdateAlert extends StatefulWidget {
 }
 
 class _AlertState extends State<PhoneNumberUpdateAlert> {
+  // Create an instance of the ProfileController for managing profile-related actions
   final ProfileController _profileController = ProfileController();
-  // Form key for managing the state of the login form
+
+  // Form key for managing the state of the phone number update form
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
-  // Variables to manage the visibility of the password
+  // Variable to manage the visibility of the password
   bool _isObscure = true;
 
-  // Variables to store user email and password inputs
+  // Variables to store user's new phone number and password
   String _phoneNumber = "";
   String _password = "";
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      // Adjust the padding, content alignment, and size of the alert dialog
       buttonPadding: const EdgeInsets.all(16),
-      icon: const Icon(
-        Icons.update,
-      ),
       contentPadding: const EdgeInsets.symmetric(horizontal: 16),
       titlePadding: const EdgeInsets.symmetric(vertical: 16),
       scrollable: false,
       semanticLabel: "alert dialog for updating the phone number",
       elevation: 24,
+      icon: const Icon(
+        Icons.update,
+      ),
       title: Text(
         "Phone Number Update",
         style: Theme.of(context).textTheme.headlineSmall!.copyWith(
@@ -42,6 +45,7 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
             ),
         textAlign: TextAlign.center,
       ),
+      // Set the content of the alert dialog with a form for new phone number and password input
       content: SizedBox(
         width: MediaQuery.sizeOf(context).width / 4 * 3,
         height: MediaQuery.sizeOf(context).height / 3,
@@ -51,6 +55,7 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // IntlPhoneField for entering the new phone number
               Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 48),
                 child: IntlPhoneField(
@@ -67,12 +72,12 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
                       newPhoneNumber!.completeNumber.toString().trim(),
                 ),
               ),
+              // TextFormField for entering the password
               TextFormField(
                 decoration: InputDecoration(
                   hintText: "Your password",
                   prefixIcon: const Icon(Icons.lock_outline),
                   suffixIcon: IconButton(
-                    // Toggle visibility of the password with an icon button
                     onPressed: () {
                       setState(() {
                         _isObscure = !_isObscure;
@@ -86,11 +91,11 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
                 obscureText: _isObscure,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return "please provide your password";
+                    return "Please provide your password";
                   }
                   // Check if the password is at least 6 characters long
                   if (value.length < 6) {
-                    return "the password  must be at least 6 characters in length";
+                    return "The password must be at least 6 characters in length";
                   }
                   return null;
                 },
@@ -100,27 +105,33 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
           ),
         ),
       ),
+      // Set the actions (buttons) for the alert dialog
       actions: [
+        // Cancel button
         ElevatedButton(
           onPressed: () {
             Navigator.pop(context);
           },
           child: const Text("Cancel"),
         ),
+        // Submit button for updating the phone number
         FilledButton(
           onPressed: () async {
             if (_formkey.currentState!.validate()) {
               _formkey.currentState!.save();
+              // Verify the entered password
               bool isVerified = await _profileController.verifyPassword(
                 entredPassword: _password,
               );
               if (isVerified) {
                 try {
+                  // Update the phone number
                   await _profileController.updatePhoneNumber(
                     newPhoneNumber: _phoneNumber,
                   );
                 } catch (e) {
                   if (context.mounted) {
+                    // Display an error message as a snackbar for phone number update failure
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text("Error: Unable to update phone number"),
@@ -131,6 +142,7 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
 
                 if (context.mounted) {
                   Navigator.pop(context);
+                  // Display a success message as a snackbar
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
                       content: Text("Phone number updated successfully"),
@@ -140,6 +152,7 @@ class _AlertState extends State<PhoneNumberUpdateAlert> {
               } else {
                 if (context.mounted) {
                   Navigator.pop(context);
+                  // Display an error message as a snackbar for wrong password
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
                       content: Text("Wrong password"),
