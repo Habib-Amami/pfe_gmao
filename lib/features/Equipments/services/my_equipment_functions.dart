1f1707c0783a0fa82a5ebd6c9637174fe9044467
Adam zahi <zahi.adem10@gmail.com>
2024-03-13 20:18:36 +0100

    adding date format function

---
+++ lib/features/Equipments/services/my_equipment_functions.dart
@@ -0,0 +1,49 @@
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+String formattedDate(timeStamp) {
+  var dateFromTimestamp =
+      DateTime.fromMicrosecondsSinceEpoch(timeStamp.seconds * 1000);
+  return DateFormat('dd-MM-yyyy').format(dateFromTimestamp);
+}
+
+Widget myProperty(String myTitle, String myValue) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 15.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            myTitle,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        Container(
+          height: 58,
+          margin: const EdgeInsets.only(top: 3),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey.shade500,
+              width: 1.3,
+            ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: TextFormField(
+            enabled: false,
+            initialValue: myValue,
+            style: const TextStyle(fontSize: 20, color: Colors.black),
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
