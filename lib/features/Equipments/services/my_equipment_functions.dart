ae1bcfbd4a7969c1a2b836939d4f20297688fe5d
Adam zahi <zahi.adem10@gmail.com>
2024-03-21 01:28:22 +0100

    fixing formatted date function's logic

--- lib/features/Equipments/services/my_equipment_functions.dart
+++ lib/features/Equipments/services/my_equipment_functions.dart
@@ -1,10 +1,59 @@
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 String formattedDate(timeStamp) {
-  var dateFromTimestamp =
-      DateTime.fromMicrosecondsSinceEpoch(timeStamp.seconds * 1000);
+  DateTime dateFromTimestamp =
+      DateTime.fromMillisecondsSinceEpoch(timeStamp.seconds * 1000);
   return DateFormat('dd-MM-yyyy').format(dateFromTimestamp);
+}
+
+Widget showState(String data) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      const Text(
+        "Status:",
+        style: TextStyle(fontWeight: FontWeight.w400),
+      ),
+      const SizedBox(width: 10),
+      data.toLowerCase() == 'active'
+          ? Container(
+              width: 22,
+              height: 22,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.green),
+              child: const Icon(
+                  size: 18, Ionicons.checkmark_sharp, color: Colors.white),
+            )
+          : data.toLowerCase() == 'shutdown'
+              ? Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.red, shape: BoxShape.circle),
+                  width: 22,
+                  height: 22,
+                  child: const Center(
+                    child: Icon(
+                        size: 18,
+                        Icons.power_off_outlined,
+                        color: Colors.white),
+                  ),
+                )
+              : data.toLowerCase() == 'standby'
+                  ? Container(
+                      width: 22,
+                      height: 22,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.yellow),
+                      child: const Icon(
+                        size: 18,
+                        Ionicons.pause_circle_outline,
+                        color: Colors.white,
+                      ),
+                    )
+                  : const Text("please revise the state value!")
+    ],
+  );
 }
 
 Widget myProperty(String myTitle, String myValue, Widget myIcon) {
