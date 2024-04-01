3565e481a168b2e9ca5c2e8fb96230c13996bb0e
Habib-Amami <habibamami050@gmail.com>
2024-04-01 03:06:30 +0200

    deleting usless extendtion methode on enum

--- lib/features/Equipments/model/priority_enum.dart
+++ lib/features/Equipments/model/priority_enum.dart
@@ -1,16 +1,9 @@
 // Define an enumeration for equipment priorities
 enum Priority {
   // ignore: constant_identifier_names
-  High,
+  Low,
   // ignore: constant_identifier_names
   Medium,
   // ignore: constant_identifier_names
-  Low,
-}
-
-// Extension to convert enum values to strings
-extension PriorityToString on Priority {
-  String priorityToShortString() {
-    return toString().split('.').last;
-  }
+  High,
 }
--- lib/features/Equipments/model/status_enum.dart
+++ lib/features/Equipments/model/status_enum.dart
@@ -1,15 +1,8 @@
 enum Status {
   // ignore: constant_identifier_names
-  Active,
-  // ignore: constant_identifier_names
   Standby,
   // ignore: constant_identifier_names
+  Active,
+  // ignore: constant_identifier_names
   Shutdown
-}
-
-// Extension to convert enum values to strings
-extension StatusToString on Status {
-  String statusToShortString() {
-    return toString().split('.').last;
-  }
 }
