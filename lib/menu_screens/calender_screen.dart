668355281c87729608345bae1a3574bb6c460aed
Adam zahi <zahi.adem10@gmail.com>
2024-05-09 14:51:30 +0100

    showing the following intervention base on selected day

--- lib/menu_screens/calender_screen.dart
+++ lib/menu_screens/calender_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pfe_gmao/features/interventions/model/data_models/intervntion.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalenderScreen extends StatefulWidget {
@@ -9,11 +13,12 @@ class CalenderScreen extends StatefulWidget {
 }
 
 class _CalenderScreenState extends State<CalenderScreen> {
-  // today format
-  DateTime today = DateTime.now();
-  void _onDaySelected(DateTime day, DateTime focusedDay) {
+  //today format
+  DateTime selectedDay = DateTime.now();
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
-      today = day;
+      // selectedDay = day;
+      this.selectedDay = selectedDay;
     });
   }
 
@@ -25,7 +30,7 @@ class _CalenderScreenState extends State<CalenderScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(9.0),
+          padding: const EdgeInsets.only(left: 9.0, right: 9, bottom: 9),
           child: Column(
             children: [
               TableCalendar(
@@ -34,11 +39,11 @@ class _CalenderScreenState extends State<CalenderScreen> {
                   titleCentered: true,
                   formatButtonVisible: true,
                 ),
-                focusedDay: today,
+                focusedDay: selectedDay,
                 firstDay: DateTime.utc(2024, 01, 01),
                 lastDay: DateTime.utc(2100),
                 availableGestures: AvailableGestures.all,
-                selectedDayPredicate: (day) => isSameDay(today, day),
+                selectedDayPredicate: (day) => isSameDay(selectedDay, day),
                 onDaySelected: _onDaySelected,
                 calendarFormat: _calendarFormat,
                 onFormatChanged: (format) {
@@ -47,7 +52,88 @@ class _CalenderScreenState extends State<CalenderScreen> {
                   });
                 },
               ),
-              const Divider(),
+
+              //const Divider(),
+              Expanded(
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('interventions')
+                        .where('interventionDate',
+                            isEqualTo:
+                                selectedDay.toIso8601String().split('T').first)
+                        .snapshots(),
+                    builder: ((context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.none) {
+                        return const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.error_outline,
+                                color: Colors.red,
+                                size: 50.0,
+                              ),
+                              SizedBox(height: 10.0),
+                              Text("Lost connection"),
+                            ],
+                          ),
+                        );
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CircularProgressIndicator(),
+                              Text("Loading Notifications ...")
+                            ],
+                          ),
+                        );
+                      }
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // const CircularProgressIndicator(),
+                              Center(
+                                child: Text('Error: ${snapshot.error}'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      // data found
+                      List<Intervention> interventions = snapshot.data!.docs
+                          .map(
+                            (document) => Intervention.fromJson(
+                              document.data(),
+                            ),
+                          )
+                          .toList();
+                      if (interventions.isEmpty) {
+                        return Center(
+                          child: Text(
+                            'No intervention available this day!',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        );
+                      }
+                      return ListView.builder(
+                          itemCount: interventions.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              color: Colors.red[100],
+                              child: ListTile(
+                                title:
+                                    Text(interventions[index].equipmentTagName),
+                                subtitle:
+                                    Text(interventions[index].interventionType),
+                              ),
+                            );
+                          });
+                    })),
+              )
             ],
           ),
         ),
