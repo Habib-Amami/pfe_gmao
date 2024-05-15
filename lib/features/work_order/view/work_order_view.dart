733195992a41ad25a16918697e26a11a6d282e22
Adam zahi <zahi.adem10@gmail.com>
2024-05-16 00:04:03 +0100

    design example for work order

--- lib/features/work_order/view/work_order_view.dart
+++ lib/features/work_order/view/work_order_view.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pfe_gmao/features/interventions/view/widget/intervention_card.dart';
 
-class WorkOrderView extends StatelessWidget {
+class WorkOrderView extends StatefulWidget {
   const WorkOrderView({super.key});
 
   @override
+  State<WorkOrderView> createState() => _WorkOrderViewState();
+}
+
+class _WorkOrderViewState extends State<WorkOrderView> {
+  DateTime today = DateTime.now();
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Work Order View"),
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: CalendarCard(
+                title: '',
+                subtitle: 'Mechanical equipment',
+                date: today,
+                typeOfCard: 'work order',
+                status: 'planned',
+                equipmentName: 'SN-201'),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
