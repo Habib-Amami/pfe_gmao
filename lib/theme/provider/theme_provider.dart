bbfc31986bf80d8cc79463edf754b724c23c64b3
Adam zahi <zahi.adem10@gmail.com>
2024-05-16 01:59:36 +0100

    maintaining the app theme

--- lib/theme/provider/theme_provider.dart
+++ lib/theme/provider/theme_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pfe_gmao/theme/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeData _themeData = lightTheme;
@@ -11,11 +12,29 @@ class ThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+  void _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    _themeData = isDarkMode ? darkTheme : lightTheme;
+    notifyListeners();
+  }
+
+  void _saveTheme(bool isDarkMode) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', isDarkMode);
+  }
+
   void toggleTheme() {
     if (_themeData == lightTheme) {
       themeData = darkTheme;
+      _saveTheme(true);
     } else {
       themeData = lightTheme;
+      _saveTheme(false);
     }
+    notifyListeners();
   }
 }
