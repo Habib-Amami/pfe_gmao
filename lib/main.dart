84f23fc0888a2f68df39925c1c3fb497c15fb7b6
Adam zahi <zahi.adem10@gmail.com>
2024-05-16 02:00:23 +0100

    making the main widget stateless

--- lib/main.dart
+++ lib/main.dart
@@ -104,14 +104,9 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
