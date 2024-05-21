716f18d74d2de0e4535c8687755765ea947a496c
Habib-Amami <habibamami050@gmail.com>
2024-05-21 15:07:43 +0200

    changing the notification view to a tab view

---
+++ lib/features/notifications/view/tabs/workorder_notification_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WorkorderNotificationsTab extends StatelessWidget {
+  const WorkorderNotificationsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("expa works"),
+    );
+  }
+}
