836695d4a1bc7c0fe475eb731ddae13b2fb520db
Habib-Amami <habibamami050@gmail.com>
2024-06-02 09:07:31 +0200

    adding fade transition the location alert

--- lib/features/Equipments/View/widgets/alerts/equipment_location_permission_denied_alert.dart
+++ lib/features/Equipments/View/widgets/alerts/equipment_location_permission_denied_alert.dart
@@ -1,32 +1,68 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 // Alert dialog widget displayed when location permission is denied
-class EquipmentLocationPermissionDeniedAlert extends StatelessWidget {
+class EquipmentLocationPermissionDeniedAlert extends StatefulWidget {
   const EquipmentLocationPermissionDeniedAlert({super.key});
 
   @override
+  State<EquipmentLocationPermissionDeniedAlert> createState() =>
+      _EquipmentLocationPermissionDeniedAlertState();
+}
+
+class _EquipmentLocationPermissionDeniedAlertState
+    extends State<EquipmentLocationPermissionDeniedAlert>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration:
+          const Duration(milliseconds: 600), // Adjust the duration as needed
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text("Permission denied !"),
-      content: const Text(
-        "you can't locate an equipment, please change the location permission from the settings",
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context), // Close the dialog
-          child: const Text("cancel"),
+    return FadeScaleTransition(
+      animation: _animation,
+      child: AlertDialog(
+        title: const Text("Permission denied !"),
+        content: const Text(
+          "you can't locate an equipment, please change the location permission from the settings",
         ),
-        TextButton(
-          onPressed: () => openAppSettings().then(
-            (_) => Navigator.pop(
-                context), // Open app settings and close the dialog
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context), // Close the dialog
+            child: const Text("cancel"),
           ),
-          child: const Text(
-            "Settings",
-          ),
-        )
-      ],
+          TextButton(
+            onPressed: () => openAppSettings().then(
+              (_) => Navigator.pop(
+                  context), // Open app settings and close the dialog
+            ),
+            child: const Text(
+              "Settings",
+            ),
+          )
+        ],
+      ),
     );
   }
 }
