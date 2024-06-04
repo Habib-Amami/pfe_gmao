5b1ba179ccbb6f81988e252882a3b37c9063f108
Adam zahi <zahi.adem10@gmail.com>
2024-06-04 01:30:34 +0100

    Disable displaying user info in console

--- lib/features/interventions/view/calender_screen.dart
+++ lib/features/interventions/view/calender_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -42,9 +41,6 @@ class _CalenderScreenState extends State<CalenderScreen> {
         user = UserModel.fromFirestore(snapshot, null);
       },
     );
-    if (kDebugMode) {
-      print(user);
-    }
     return user.role == Roles.Administrator;
   }
 
