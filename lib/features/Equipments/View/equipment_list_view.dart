415bfd0210394deb8434d8479b10e65c25cbc9b8
Habib-Amami <habibamami050@gmail.com>
2024-06-06 15:17:34 +0200

    change in the animation

--- lib/features/Equipments/View/equipment_list_view.dart
+++ lib/features/Equipments/View/equipment_list_view.dart
@@ -84,7 +84,7 @@ class EquipmentScreenState extends State<EquipmentScreen> {
       floatingActionButton: isAdmin
           ? OpenContainer(
               transitionType: ContainerTransitionType.fadeThrough,
-              transitionDuration: const Duration(milliseconds: 400),
+              // transitionDuration: const Duration(milliseconds: 200),
               closedElevation: 6.0,
               closedShape: const RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(Radius.circular(16.0)),
