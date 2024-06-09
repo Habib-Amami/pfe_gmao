b8dcd1509a59a35379e2bb80c02dc7889e7510cc
Habib-Amami <habibamami050@gmail.com>
2024-06-09 21:23:18 +0200

    updating email regex for the login form

--- lib/features/authentication/login/view/login_view.dart
+++ lib/features/authentication/login/view/login_view.dart
@@ -101,6 +101,24 @@ class _LoginViewState extends State<LoginView> {
                         if (value == null || value.isEmpty) {
                           return "please provide an email";
                         }
+                        // r'''^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+   # Local part of the email before @
+                        //   (?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)* # Additional parts of local part separated by dots
+                        //   |                                   # OR
+                        //   "(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f  # Quoted string part
+                        //   |\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")  # Escaped characters in quoted string
+                        //   @                                   # At symbol separating local and domain parts
+                        //   (?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?  # First part of the domain
+                        //   \.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?   # Additional domain parts
+                        //   |\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}   # OR IPv4 address part
+                        //   (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?                 # Final part of IPv4 address
+                        //   |[a-z0-9-]*[a-z0-9]:                                     # OR IPv6 address part
+                        //   (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f        # Characters allowed in IPv6
+                        //   |\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$''';              # Closing brackets for IPv6 and email
+                        if (!RegExp(
+                                r'''^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$''')
+                            .hasMatch(value)) {
+                          return 'Invalid email format';
+                        }
                         return null;
                       },
                       onSaved: (newValue) {
